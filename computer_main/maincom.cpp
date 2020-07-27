7aad8a392e0c14f5974f68347ee5c574f481eaa3
HYEEWON <38900338+HYEEWON@users.noreply.github.com>
2020-07-27 23:48:46 +0900

    Update maincom.cpp
    erase useless code

--- computer_main/maincom.cpp
+++ computer_main/maincom.cpp
@@ -63,84 +63,13 @@ int main(int argc, char *argv[])
 
 
 	//thread cv_s(calculation3D, cam1_2d, cam2_2d, RT[0], RT[1], mtx[0], mtx[1]);//thread cv_s(transformation); //thread cv_s(opencvmain);
-	thread gl_s(glfmain, argc, argv); //thread gl_s(glfmain, argc, argv);
+	thread gl_s(glfmain, argc, argv); 
 	//Point2d cam1_2d, cam2_2d;
 	struct udpparameter p1;	
 	
 	p1 = udpsocket();
 	//printf("struct s %d\n", p1.s);
 	thread get_point_thread(&upd_thread, &p1);
-	//get_point_thread.detach();
-
-	//while (1) {
-	//	
-	//	
-	//	//p1 = udpreceive(&p1);
-	//	//if (p1.arr[3] == 1)
-	//	//{
-	//	//	cam1_2d.x = p1.arr[1];
-	//	//	cam1_2d.y = p1.arr[2];
-	//	//	
-	//	//}
-	//	//if (p1.arr[3] == 2)
-	//	//{
-	//	//	cam2_2d.x = p1.arr[1];
-	//	//	cam2_2d.y = p1.arr[2];
-	//	//}
-	//	//cout << "recieve" << p1.arr[1] << p1.arr[2] << endl;
-	//	
-	//	//cout << cam1_2d << endl;
-	//	//cout << cam2_2d << endl;
-	//	//point_lock.lock();
-	//	//cal_2d1.x = cam_2d[0][1].x;
-	//	//cal_2d1.y = cam_2d[0][1].y;
-	//	//cal_2d2.x = cam_2d[1][1].x;
-	//	//cal_2d2.y = cam_2d[1][1].y;
-	//	//point_lock.unlock();
-
-	//	//Point_3D = calculation3D(cal_2d1, cal_2d2, RT[0], RT[1], mtx[0], mtx[1]);
-	//	//printf("print 3dlocation %lf %lf %lf\n", Point_3D.x, Point_3D.y, Point_3D.z);
-	//	/* neck[0] = Point_3D.x;
-	//	 neck[1] = Point_3D.y;
-	//	 neck[2] = Point_3D.z;
-	//	 cout << neck[0] << " " << neck[1] << " " << neck[2] << endl;*/
-
-
-
-
-
-	//	//if (coorpi1 == 1 && coorpi2 == 1)////////////////////////////////////////////////////////////////////////
-	//	//{
-
-	//		//cv_s.join();
-	//		//gl_s.join();
-	//	//{
-	//	//printmenu();
-
-	//	/*switch (tcpcommenu(argc, argv))
-	//	{
-	//	case 1: //pose
-	//		tcpcommain(argc, argv);
-	//		if (infopi[0] == 1 && infopi[1] == 1)
-	//		{
-	//			for (int j = 0; j < 2; ++j)
-	//			{
-	//				convertToMat(buf[i], mtx[i], dist[i], R[i], T[i]);
-	//			}
-	//			cout << "Info SUCCESS" << endl;
-	//		}
-	//		break;
-	//	case 2: //tracking
-	//		//udpÅ°°í
-	//		//cv_s.join();
-	//		//gl_s.join();
-	//		break;
-	//	case 3: //finish
-	//		closesocket(s);
-	//		WSACleanup();
-	//		return 0;
-	//	}*/
-	//}
 	gl_s.join();
 	get_point_thread.join();
 	return 0;
