c3d25d34ab8a6ba65f37985e55c7430c5e5b6d01
HYEEWON <parkhyeewon@gmail.com>
2021-01-14 19:50:47 +0900

    Raspberry Pi Calibration

---
+++ RaspberryPiCalibration/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <thread>
+#include <sstream>
+#include <string>
+#include <ctime>
+#include <cstdio>
+
+#include <opencv2/core.hpp>
+#include <opencv2/core/utility.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include "calibration.h"
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	calibrationMain();
+	return 0;
+}
